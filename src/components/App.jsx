ef60cfd3f3bbc6a898aa03b926548d2d0afe1d6e
Esther García <garbennes@gmail.com>
2024-02-19 00:48:57 +0100

    Add loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 import logo from "../images/Harry_Potter_wordmark.svg";
 
+import Loader from "./Loader";
 import HomePage from "./HomePage";
 import MessagePage from "./MessagePage";
 import CharacterDetail from "./CharacterDetail";
@@ -12,39 +13,50 @@ import "../scss/App.scss";
 import { fetchCharacters } from "../services/fetch";
 
 function App() {
-
   const [data, setData] = useState([]);
+  // Add loading state
+  const [isLoading, setIsLoading] = useState(false); 
   const [dataFilters, setDataFilters] = useState({
     name: "",
     house: "Gryffindor",
   });
 
   useEffect(() => {
+    setIsLoading(true);
     fetchCharacters().then((responseData) => {
-      const sortedData = responseData.sort((a, b) => a.name.localeCompare(b.name));
+      const sortedData = responseData.sort((a, b) =>
+        a.name.localeCompare(b.name)
+      );
       setData(sortedData);
+      setIsLoading(false);
     });
   }, []);
 
-  const handleFilterCharacter = (filter, value) => {
-    setDataFilters({ ...dataFilters, [filter]: value });
+  if (isLoading) {
+    return <Loader />
   }
 
+  const handleFilterCharacter = (filter, value) => {
+    setDataFilters({ ...dataFilters, [filter]: value });
+  };
+
   const filteredCharacters = data
-  .filter((character) =>
-    character.name.toLowerCase().includes(dataFilters.name.toLowerCase())
-  )
-  .filter((character) => {
-    if (dataFilters.house === "All") {
-      return true;
-    } else {
-      return character.house === dataFilters.house;
-    }
-  });
+    .filter((character) =>
+      character.name.toLowerCase().includes(dataFilters.name.toLowerCase())
+    )
+    .filter((character) => {
+      if (dataFilters.house === "All") {
+        return true;
+      } else {
+        return character.house === dataFilters.house;
+      }
+    });
 
   const getInfoCharacter = (characterName, characterAncestry) => {
     const characterFound = data.find(
-      (character) => character.name.toLowerCase() === characterName.toLowerCase() && character.ancestry === characterAncestry
+      (character) =>
+        character.name.toLowerCase() === characterName.toLowerCase() &&
+        character.ancestry === characterAncestry
     );
     return characterFound;
   };
@@ -58,13 +70,13 @@ function App() {
   };
 
   return (
-  <div className="container">
-    <header className="header">
-      <img className="header__logo" src={logo} alt="Logo Harry Potter" />
-    </header>
-    <main className="main">
-      <Routes>
-        <Route
+    <div className="container">
+      <header className="header">
+        <img className="header__logo" src={logo} alt="Logo Harry Potter" />
+      </header>
+      <main className="main">
+        <Routes>
+          <Route
             exact
             path="/"
             element={
@@ -87,19 +99,19 @@ function App() {
             element={<CharacterDetail getInfoCharacter={getInfoCharacter} />}
           />
 
-          <Route path="*" element={
-            <MessagePage text="Lo que estás buscando no se ha podido encontrar" />
-            } 
+          <Route
+            path="*"
+            element={
+              <MessagePage text="Lo que estás buscando no se ha podido encontrar" />
+            }
           />
-
-      </Routes>
-    </main>
-    <footer className="footer">
-      <small className="footer__copy"></small>
-    </footer>
-  </div>
+        </Routes>
+      </main>
+      <footer className="footer">
+        <small className="footer__copy"></small>
+      </footer>
+    </div>
   );
-
 }
 
 export default App;
