d973d3f3c3ea375c1139f6b73b75ad7e52d28257
Esther García <garbennes@gmail.com>
2024-02-18 22:42:05 +0100

    Add translation file

---
+++ src/services/translations.js
@@ -0,0 +1,48 @@
+// Translations
+
+const translation = {
+  "male" : "Hombre",
+  "female" : "Mujer",
+  "human": {
+    male: "Humano",
+    female: "Humana"
+  },
+  "half-human": {
+    male: "Semihumano",
+    female: "Semihumana"
+  },
+  "acromantula": "Acromántula",
+  "centaur": "Centauro",
+  "ghost": "Fantasma",
+  "goblin": "Goblin",
+  "hippogriff": "Hipogrifo",
+  "house-elf": "Elfo doméstico",
+  "werewolf": "Hombre lobo",
+  "three-headed dog": "Perro de tres cabezas",
+  "giant": "Gigante",
+  "owl": "Búho",
+  "half-giant": "Semigigante",
+  "cat": "Gato",
+  "dragon": "Dragón",
+  "poltergeist": "Poltergeist",
+  "vampire":  {
+    male: "Vampiro",
+    female: "Vampira"
+  },
+  "half-blood" : {
+    male: "Mestizo",
+    female: "Mestiza"
+  },
+  "pure-blood" : "Sangre pura",
+  "muggle" : "Muggle",
+  "half-veela": "Mitad Veela",
+  "squib" : 'Squib',
+  "muggleborn" : {
+    male: "Nacido de muggles",
+    female: "Nacida de muggles"
+  },
+  "quarter-veela" : "Cuarto de Veela",
+  "" : "?"
+};
+
+export default translation;
