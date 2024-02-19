39317c0e484ce1c12f494a0d0b002c4639d70f3b
Esther García <garbennes@gmail.com>
2024-02-19 20:35:54 +0100

    Divide Filters into smaller components

---
+++ src/components/FilterGender.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+
+function FilterGender({ handleFilterCharacter, valueGender }) {
+
+  const handleInputGender = (event) => {
+    handleFilterCharacter("gender", event.currentTarget.value);
+  };
+
+  return (
+    <fieldset className="filters__check check">
+    <input 
+      type="radio" 
+      id="hombre" 
+      name="gender"
+      value="male"
+      checked={valueGender === "male"}
+      onChange={handleInputGender}
+    />
+    <label className="check__label" htmlFor="hombre">Hombre</label>
+    <input 
+      type="radio" 
+      id="mujer" 
+      name="gender"
+      value="female"
+      checked={valueGender === "female"}
+      onChange={handleInputGender} 
+    />
+    <label className="check__label" htmlFor="mujer">Mujer</label>
+  </fieldset>
+  );
+}
+
+FilterGender.propTypes = {
+  handleFilterCharacter: PropTypes.func.isRequired,
+  valueGender: PropTypes.string.isRequired,
+};
+
+
+export default FilterGender
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,18 +1,11 @@
 import PropTypes from "prop-types";
 
-function Filters({ handleFilterCharacter, handleClickReset, valueName, valueHouse, valueGender }) {
+import FilterName from "./FilterName";
+import FilterHouse from "./FilterHouse";
+import FilterGender from "./FilterGender";
+import FiltersReset from "./FiltersReset";
 
-  const handleInputName = (event) => {
-    handleFilterCharacter("name", event.currentTarget.value);
-  };
-
-  const handleInputHouse = (event) => {
-    handleFilterCharacter("house", event.currentTarget.value);
-  };
-
-  const handleInputGender = (event) => {
-    handleFilterCharacter("gender", event.currentTarget.value);
-  };
+function Filters({ handleFilterCharacter, handleClickReset, valueName, valueHouse, valueGender}) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -20,68 +13,21 @@ function Filters({ handleFilterCharacter, handleClickReset, valueName, valueHous
 
   return (
     <form className="filters" onSubmit={handleSubmit}>
-      <fieldset className="filters__name name">
-        <label className="name__label" htmlFor="name">
-          Busca por personaje:
-        </label>
-        <input
-          className="input"
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Ej: Harry Potter"
-          onInput={handleInputName}
-          value={valueName}
-        />
-      </fieldset>
-      <fieldset className="filters__houses houses">
-        <label htmlFor="houses__label">Selecciona la casa:</label>
-        <select
-          className="select"
-          name="houses__select"
-          onInput={handleInputHouse}
-          value={valueHouse}
-        >
-          <option className="houses__option" value="All">
-            Todas
-          </option>
-          <option className="houses__option" value="Gryffindor">
-            Gryffindor
-          </option>
-          <option className="houses__option" value="Ravenclaw">
-            Ravenclaw
-          </option>
-          <option className="houses__option" value="Hufflepuff">
-            Hufflepuff
-          </option>
-          <option className="houses__option" value="Slytherin">
-            Slytherin
-          </option>
-        </select>
-      </fieldset>
-      <fieldset className="filters__check check">
-        <input 
-          type="radio" 
-          id="hombre" 
-          name="gender"
-          value="male"
-          checked={valueGender === "male"}
-          onChange={handleInputGender}
-        />
-        <label className="check__label" htmlFor="hombre">Hombre</label>
-        <input 
-          type="radio" 
-          id="mujer" 
-          name="gender"
-          value="female"
-          checked={valueGender === "female"}
-          onChange={handleInputGender} 
-        />
-        <label className="check__label" htmlFor="mujer">Mujer</label>
-      </fieldset>
-      <button className="filters__reset" onClick={handleClickReset}>
-        <i className="fa-solid fa-rotate-left"></i>
-      </button>
+      <FilterName 
+        handleFilterCharacter={handleFilterCharacter}
+        valueName={valueName}
+      />
+      <FilterHouse
+        handleFilterCharacter={handleFilterCharacter}
+        valueHouse={valueHouse}
+      />
+      <FilterGender 
+        handleFilterCharacter={handleFilterCharacter}
+        valueGender={valueGender}
+      />
+      <FiltersReset 
+        handleClickReset={handleClickReset}
+      /> 
     </form>
   );
 }
